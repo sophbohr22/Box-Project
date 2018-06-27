019248222d405b03bb4b4487f3e948e014de9c6c
Kate Martino <kate.m1648@gmail.com>
2018-06-27 11:47:37 -0500

    edited the delete branch

--- BoxProblem/Repositories/BoxRepository.cs
+++ BoxProblem/Repositories/BoxRepository.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BoxProblem.Data;
 
 namespace BoxProblem.Repositories
 {
     public class BoxRepository
     {
-    }
+        private ApplicationDbContext dbContext;
+
+        public BoxRepository(ApplicationDbContext context)
+        {
+            dbContext = context;
+        }
+
+        public List<BoxInventory> GetAllBoxes()
+        {
+            return dbContext.Boxes.ToList();
+        }
+
+        public void DeleteBox(BoxInventory toDelete)
+        {
+            dbContext.Boxes.Remove(toDelete);
+            dbContext.SaveChanges();
+
+        }
+
 }
--- BoxProblem/Services/BoxService.cs
+++ BoxProblem/Services/BoxService.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BoxProblem.Data;
+using BoxProblem.Repositories; 
 
 namespace BoxProblem.Services
 {
     public class BoxService
     {
+        private BoxRepository repository;
+
+
+        public BoxService(ApplicationDbContext context)
+        {
+            repository = new BoxRepository(context);
+        }
+
+        public List<BoxInventory> GetAllBoxes()
+        {
+            return repository.GetAllBoxes();
+        }
+
+        public void DeleteBox(Box toDelete)
+        {
+            repository.DeleteBox(toDelete);
+        }
     }
 }
